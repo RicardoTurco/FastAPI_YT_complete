638ad007e696dfa834fea9675c5930a4724bd25a
RicardoTurco <ricturco@gmail.com>
2021-12-18 18:24:24 -0300

    add: main file

---
+++ main.py
@@ -0,0 +1,38 @@
+import uvicorn
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: int
+    quantidade: int
+    descricao: str
+    valor: float
+
+
+app = FastAPI()
+
+
+banco_de_dados = []
+
+
+@app.post("/item")
+def add_item(item: Item):
+    banco_de_dados.append(item)
+    return item
+
+
+@app.get("/item")
+def list_item():
+    return banco_de_dados
+
+
+@app.get("/item/valor-total")
+def get_valor_total():
+    valor_total = sum([item.valor *  item.quantidade for item in banco_de_dados])
+    return {"valor_total": valor_total}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
