6264a81636d9e6ae60c6b49f594e149631896d7e
RicardoTurco <ricturco@gmail.com>
2021-12-19 09:45:45 -0300

    add: Tests to validate 'sigla' of PAPEIS

--- tests/api/test_papeis.py
+++ tests/api/test_papeis.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from tests.api.utils.papeis import create_papel_papel_valido
+from tests.api.utils.papeis import create_papel_papel_valido, create_papel_invalido
 
 
 def test_cria_papel(client: TestClient) -> None:
@@ -8,3 +8,10 @@ def test_cria_papel(client: TestClient) -> None:
     content = response.json()
     assert response.status_code == 200
     assert content["cnpj"] == body["cnpj"]
+
+
+def test_cria_papel_com_sigla_invalida(client: TestClient) -> None:
+    body = create_papel_invalido(['sigla'])
+    response = client.post("/papeis/", json=body)
+    content = response.json()
+    assert response.status_code == 422
--- tests/api/utils/papeis.py
+++ tests/api/utils/papeis.py
@@ -5,3 +5,17 @@ def create_papel_papel_valido():
         "sigla": "PETR4",
         "cnpj": "string",
     }
+
+
+def create_papel_invalido(campos_invalidos=["sigla"]):
+    papel_invalido = {
+        "id": 0,
+        "nome": "string",
+        "sigla": "PETR4",
+        "cnpj": "string",
+    }
+
+    if "sigla" in campos_invalidos:
+        papel_invalido["sigla"] = "AAAAAAAA"
+
+    return papel_invalido
